f94453553462bd059c802df564a8f87435b0a2b3
Solomon Udofia <meetsolomonudofia@gmail.com>
2024-09-15 15:30:02 -0700

    Created class to handle session

--- app/Core/session.php
+++ app/Core/session.php
@@ -1,7 +1,7 @@
 <?php
 namespace App\Core;
 
-class session 
+class Session 
 {
     public function __construct()
     {
--- index.php
+++ index.php
@@ -1,7 +1,11 @@
 <?php
-
+use App\Core\Session;
 require 'vendor/autoload.php';
 
-echo '<pre>';
-var_dump(App\Core\Request::uri());
-echo '</pre>';
+// echo '<pre>';
+// var_dump(App\Core\Request::uri());
+// echo '</pre>';
+
+$session = new Session;
+$session->set('name', 'John Doe');
+echo $session->get('name');
